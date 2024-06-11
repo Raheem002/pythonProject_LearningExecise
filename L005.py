de58f137930c0506a9cc6cb2256baebefb0f03e4
RAHEEM002 <raheemkhan577@gmail.com>
2024-06-11 20:17:36 +0530

    Types for string

---
+++ L005.py
@@ -0,0 +1,22 @@
+# In Builtin Functions
+# print(), input(), max(), min(), type(), format(), len(), sum(), avg()
+
+# string IMPORTANT
+name = "Amit"
+print(name)
+print(type(name))  # type
+print(id(name))  # Memory address where it is stored
+print(len(name))  # lenth of the name
+name = name.upper()  # Convert the name in to upper case
+print(name)
+name = name.lower()  # Convert the name in to lower case
+print(name)
+name = name.count('t') #Show the count of mentioned letter in the name
+print(name)
+
+rau = "over"
+print(rau[3])
+#Python is immutable - that can't be changed
+
+rau[1]="w"
+print(rau) #in the python interpreter we cannot replace the value but we can change the value
