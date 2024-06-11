50df963e6900dd3b1f620c3fb6378229f672b708
RAHEEM002 <raheemkhan577@gmail.com>
2024-06-11 09:09:48 +0530

    Task project to Add the input number from user

---
+++ L003.py
@@ -0,0 +1,18 @@
+#Take 2 integer number from user and add them
+
+#number1 = input('Give a number to add')
+#number2 = input('Give a number to add')
+#result = number1 + number2
+#print(result)
+
+#Now here it will act as concatination so we have to conter the str to int
+
+#We have to convert the string in to integer by using int() command
+
+number1 = input('Give a number to add')
+number2 = input('Give a number to add')
+result = int(number1) + int(number2)
+print(result)
+
+# '+' act as sum in the integers
+# '+' act as concatination in the string
