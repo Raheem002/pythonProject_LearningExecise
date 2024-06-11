c8071bc6ef41e2d052027493fa8af551310d59b3
RAHEEM002 <raheemkhan577@gmail.com>
2024-06-11 08:42:06 +0530

    L002test

--- L002.py
+++ L002.py
@@ -1,2 +1,4 @@
-print(12)
-print(13)
+#Take a input from the username
+first_name = input('Enter Your first Name')
+last_name = input('Enter Your last Name')
+print('What is your first name', first_name,'Enter your last name', last_name)
