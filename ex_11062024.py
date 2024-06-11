cfa5f0ab0c70c79dc26bef928fa46d82e374181a
RAHEEM002 <raheemkhan577@gmail.com>
2024-06-11 08:50:30 +0530

    Variable types

---
+++ ex_11062024.py
@@ -0,0 +1,18 @@
+#Variable
+#String, Integer, Boolean, Complex, Float
+
+a = 'the'
+print(a)
+print(type(a))
+
+b = 123
+print(b)
+print(type(b))
+
+c = True
+print(c)
+print(type(c))
+
+d = 3.98
+print(d)
+print(type(d))
